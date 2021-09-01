51c88ac39d5e3040d9f7bc89adff9f5e3ea15d87
azizulwd <azizulislamgd@gmail.com>
2021-09-02 02:54:01 +0600

    display book function

--- js/index.js
+++ js/index.js
@@ -1,12 +1,35 @@
 
+// Function
 const loadBooks = () => {
     // Declare variable
     const searchField = document.getElementById("searchField");
     const searchFieldText = searchField.value;
+
+    // Clear
+    searchField.value = "";
     const url = `http://openlibrary.org/search.json?q=${searchFieldText}`;
 
     // Fetching data
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => displayBooks(data.docs));
+}
+
+
+const displayBooks = books => {
+    const bookContainer = document.getElementById("book_container");
+    // console.log(books);
+    books.forEach(book => {
+        console.log(book);
+        const createDiv = document.createElement("div");
+        createDiv.classList.add("book_card");
+        createDiv.innerHTML = `
+            <h2>${book.title}</h2>
+            <p><span style="font-weight: bold">Author Name: </span>${book.author_name}</p>
+            <p><span style="font-weight: bold">First Publish: </span>${book.first_publish_year}</p>
+            <p><span style="font-weight: bold">Publisher: </span>${book.publisher[0]}</p>
+        `;
+        bookContainer.appendChild(createDiv);
+    });
+    
 }
