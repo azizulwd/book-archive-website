a0b23692b9fdc2f67d2270da8e89f2220a27bfcb
azizulwd <azizulislamgd@gmail.com>
2021-09-02 18:19:10 +0600

    complete basic requirement

--- js/index.js
+++ js/index.js
@@ -7,37 +7,40 @@ const loadBooks = () => {
 
     // Clear
     searchField.value = "";
-    const url = `http://openlibrary.org/search.json?q=${searchFieldText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchFieldText}`;
 
     // Fetching data
     fetch(url)
     .then(res => res.json())
-    .then(data => displayBooks(data.docs));
+    .then(data => displayBooks(data));
 }
 
 
 const displayBooks = books => {
+    // console.log(books);
+    const allBooks = books.docs;
+    const showingBooks = document.getElementById("showingBooks");
     const bookContainer = document.getElementById("book_container");
     const erroMsg = document.getElementById("errorMsg");
 
     // Error handling
-    if(books.length === 0){
+    if(allBooks.length === 0){
         erroMsg.innerText = "No Result Found";
     }
     else{
-        erroMsg.innerText = "";
+        showingBooks.innerText = `Total Books ${books.numFound}, showing result ${allBooks.length}`;
     }
 
     // console.log(books);
-    books.forEach(book => {
+    allBooks.forEach(book => {
         console.log(book);
         const createDiv = document.createElement("div");
         createDiv.classList.add("book_card");
         createDiv.innerHTML = `
             <h2>${book.title}</h2>
-            <p><span style="font-weight: bold">Author Name: </span>${book.author_name[0]}</p>
+            <p><span style="font-weight: bold">Author Name: </span>${book.author_name}</p>
             <p><span style="font-weight: bold">First Publish: </span>${book.first_publish_year}</p>
-            <p><span style="font-weight: bold">Publisher: </span>${book.publisher[0]}</p>
+            <p><span style="font-weight: bold">Publisher: </span>${book.publisher}</p>
         `;
 
         bookContainer.appendChild(createDiv);
